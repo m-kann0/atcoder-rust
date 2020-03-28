1a4621ae27cc228b05a494dd94e381f90be79a5d
kanno <m.kanno.0915@gmail.com>
2020-03-29 02:13:04 +0900

    editorial AC

--- examples/abc160_d.rs
+++ examples/abc160_d.rs
@@ -1,5 +1,5 @@
 use std::io::Read;
-use std::collections::{VecDeque, HashMap};
+use std::cmp::min;
 
 fn main() {
     let mut buf = String::new();
@@ -13,63 +13,25 @@ fn main() {
 fn solve(input: &str) -> String {
     let mut iterator = input.split_whitespace();
 
-    let n: usize = iterator.next().unwrap().parse().unwrap();
-    let x: usize = iterator.next().unwrap().parse().unwrap();
-    let y: usize = iterator.next().unwrap().parse().unwrap();
+    let n: isize = iterator.next().unwrap().parse().unwrap();
+    let x: isize = iterator.next().unwrap().parse().unwrap();
+    let y: isize = iterator.next().unwrap().parse().unwrap();
 
-    let mut map: Vec<Vec<bool>> = vec![vec![false; n]; n];
-    for i in 0..(n - 1) {
-        map[i][i + 1] = true;
-        map[i + 1][i] = true;
-    }
-    map[x - 1][y - 1] = true;
-    map[y - 1][x - 1] = true;
+    let mut counts: Vec<usize> = vec![0; (n + 1) as usize];
 
-    let mut counts: HashMap<usize, usize> = HashMap::with_capacity(n);
-    for i in 0..n {
-        bfs(&map, i, &mut counts);
+    for i in 1..n {
+        for j in i + 1 .. n + 1 {
+            let mut count = j - i;
+            count = min(count, (x - i).abs() + 1 + (j - y).abs());
+            counts[count as usize] += 1;
+        }
     }
 
     let mut ans = String::new();
-    for k in 1..n {
-        let default = 0;
-        let count = counts.get(&k).unwrap_or(&default);
-        ans.push_str(&format!("{}\n", count));
+    for i in 1..n {
+        ans.push_str(&format!("{}\n", counts[i as usize]))
     }
-
     return ans.trim().to_string();
-}
-
-fn bfs(map: &Vec<Vec<bool>>, s: usize, counts: &mut HashMap<usize, usize>) {
-    let n = map.len();
-    let mut color: Vec<Color> = vec![Color::WHITE; n];
-    let mut d: Vec<usize> = vec![std::usize::MAX; n];
-
-    let mut q: VecDeque<usize> = VecDeque::new();
-    color[s] = Color::GRAY;
-    d[s] = 0;
-    q.push_back(s);
-
-    while !q.is_empty() {
-        let u = q.pop_front().unwrap();
-        for v in 0..n {
-            if map[u][v] && color[v] == Color::WHITE {
-                color[v] = Color::GRAY;
-                d[v] = d[u] + 1;
-                q.push_back(v);
-
-                if v > s {
-                    *counts.entry(d[v]).or_insert(0) += 1;
-                }
-            }
-        }
-        color[u] = Color::BLACK;
-    }
-}
-
-#[derive(Clone, PartialEq)]
-enum Color {
-    WHITE, GRAY, BLACK,
 }
 
 #[test]
