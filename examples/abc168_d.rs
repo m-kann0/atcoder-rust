2896ee12e6123664636cbb67ca4d3b8599abe8d9
kanno <m.kanno.0915@gmail.com>
2020-05-18 12:36:03 +0900

    implement by BFS

--- examples/abc168_d.rs
+++ examples/abc168_d.rs
@@ -1,6 +1,5 @@
 use std::io::Read;
-use std::cmp::Reverse;
-use std::collections::BinaryHeap;
+use std::collections::VecDeque;
 
 fn main() {
     let mut buf = String::new();
@@ -27,49 +26,27 @@ fn solve(input: &str) -> String {
 
     const INF: usize = std::usize::MAX;
 
-    let mut ans: Vec<usize> = vec![0; n];
+    let mut from: Vec<usize> = vec![INF; n];
+    from[0] = 0;
 
-    let mut d: Vec<usize> = vec![INF; n];
-    let mut color: Vec<Color> = vec![Color::WHITE; n];
-    let mut pq: BinaryHeap<Reverse<(usize, usize)>> = BinaryHeap::new();
-
-    d[0] = 0;
-    pq.push(Reverse((0, 0)));
-    color[0] = Color::GRAY;
-
-    while !pq.is_empty() {
-        let f = pq.pop().unwrap().0;
-
-        let u = f.1;
-        color[u] = Color::BLACK;
-
-        if d[u] < f.0 {
-            continue;
-        }
-
-        for j in 0..graph[u].len() {
-            let v = graph[u][j];
-            if color[v] == Color::BLACK {
-                continue;
-            }
-            if d[v] > d[u] + 1 {
-                d[v] = d[u] + 1;
-                ans[v] = u;
-                pq.push(Reverse((d[v], v)));
-                color[v] = Color::GRAY;
+    let mut q: VecDeque<usize> = VecDeque::new();
+    q.push_back(0);
+    while !q.is_empty() {
+        let u: usize = q.pop_front().unwrap();
+        for &v in graph[u].iter() {
+            if from[v] == INF {
+                from[v] = u;
+                q.push_back(v);
             }
         }
     }
 
     let mut result = String::from("Yes\n");
     for i in 1..n {
-        result.push_str(&format!("{}\n", ans[i] + 1));
+        result.push_str(&format!("{}\n", from[i] + 1));
     }
     return result.trim().to_string();
 }
-
-#[derive(Clone, PartialEq)]
-enum Color { WHITE, GRAY, BLACK }
 
 #[test]
 fn test() {
