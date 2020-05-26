2ee320c1b7bad3768c325a42a891814782524ea7
kanno <m.kanno.0915@gmail.com>
2020-05-26 10:07:20 +0900

    AC

---
+++ examples/abc133_d.rs
@@ -0,0 +1,75 @@
+use std::io::Read;
+
+fn main() {
+    let mut buf = String::new();
+    std::io::stdin().read_to_string(&mut buf).unwrap();
+
+    let answer = solve(&buf);
+
+    println!("{}", answer);
+}
+
+fn solve(input: &str) -> String {
+    let mut iterator = input.split_whitespace();
+
+    let n: usize = iterator.next().unwrap().parse().unwrap();
+    let a: Vec<usize> = (0..n).map(|_| iterator.next().unwrap().parse().unwrap()).collect();
+
+    let mut b: Vec<usize> = vec![0; n];
+    for i in 1..n {
+        b[i] = 2 * a[i - 1] - b[i - 1];
+    }
+
+    let d = (*a.last().unwrap() * 2 - *b.last().unwrap()) / 2;
+
+    for i in 0..n {
+        if i % 2 == 0 {
+            b[i] += d;
+        } else {
+            b[i] -= d;
+        }
+    }
+
+    return b.iter().map(|it| it.to_string()).collect::<Vec<String>>().join(" ");
+}
+
+#[test]
+fn test() {
+    let cases: Vec<(&str, &str)> = vec![
+        (
+            r"3
+2 2 4",
+            "4 0 4"
+        ),
+        (
+            r"5
+3 8 7 5 5",
+            "2 4 12 2 8"
+        ),
+        (
+            r"3
+1000000000 1000000000 0",
+            "0 2000000000 0"
+        ),
+    ];
+
+    let mut all_ok = true;
+    for (i, case) in cases.iter().enumerate() {
+        print!("case {} : ", i);
+
+        let expected = case.1;
+        let actual = solve(case.0);
+
+        if expected == actual {
+            println!("OK");
+        } else {
+            println!("NG");
+            println!("    Expected: {}", expected);
+            println!("    Actual  : {}", actual);
+
+            all_ok = false;
+        }
+    }
+
+    assert_eq!(all_ok, true);
+}
