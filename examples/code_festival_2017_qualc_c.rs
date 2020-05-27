0cb40b56953e45fdc4cbaa5ed3199d58c27e24a6
kanno <m.kanno.0915@gmail.com>
2020-05-28 02:11:26 +0900

    implement by editorial way

--- examples/code_festival_2017_qualc_c.rs
+++ examples/code_festival_2017_qualc_c.rs
@@ -1,4 +1,5 @@
 use std::io::Read;
+use std::collections::VecDeque;
 
 fn main() {
     let mut buf = String::new();
@@ -12,77 +13,27 @@ fn main() {
 fn solve(input: &str) -> String {
     let mut iterator = input.split_whitespace();
 
-    let s: Vec<char> = iterator.next().unwrap().chars().collect();
-
-    let mut t: Vec<char> = Vec::new();
-    let mut indexes: Vec<usize> = Vec::new();
-    for i in 0..s.len() {
-        if s[i] != 'x' {
-            t.push(s[i]);
-            indexes.push(i);
-        }
-    }
-
-    if t.len() == 0 {
-        return "0".to_string()
-    }
-
-    if !is_kaibun(&t) {
-        return "-1".to_string();
-    }
-
-    let (l, r) = if indexes.len() % 2 == 0 {
-        (indexes[indexes.len() / 2 - 1], indexes[indexes.len() / 2])
-    } else {
-        (indexes[indexes.len() / 2], indexes[indexes.len() / 2])
-    };
-    // println!("l = {}, r = {}", l, r);
-
-    // 中心から左方向に見ていく
-    let mut counts_l: Vec<isize> = Vec::new();
-    let mut c = 0;
-    for i in (0..l).rev() {
-        // println!("{}", s[i]);
-        if s[i] == 'x' {
-            c += 1;
-        } else {
-            counts_l.push(c);
-            c = 0;
-        }
-    }
-    counts_l.push(c);
-    // println!("{:?}", counts_l);
-
-    // 中心から右方向に見ていく
-    let mut counts_r: Vec<isize> = Vec::new();
-    let mut c = 0;
-    for i in (r + 1)..s.len() {
-        // println!("{}", s[i]);
-        if s[i] == 'x' {
-            c += 1;
-        } else {
-            counts_r.push(c);
-            c = 0;
-        }
-    }
-    counts_r.push(c);
-
-    // println!("{:?}", counts_r);
+    let mut s: VecDeque<char> = iterator.next().unwrap().chars().collect();
 
     let mut ans = 0;
-    for (l, r) in counts_l.iter().zip(counts_r.iter()) {
-        ans += (*l - *r).abs();
-    }
-    return ans.to_string();
-}
-
-fn is_kaibun(t: &Vec<char>) -> bool {
-    for i in 0..(t.len() / 2) {
-        if t[i] != t[t.len() - 1 - i] {
-            return false;
+    while s.len() >= 2 {
+        let f = s.front().unwrap();
+        let b = s.back().unwrap();
+        if f == b {
+            s.pop_front().unwrap();
+            s.pop_back().unwrap();
+        } else if *f == 'x' {
+            s.push_back('x');
+            ans += 1;
+        } else if *b == 'x' {
+            s.push_front('x');
+            ans += 1;
+        } else {
+            return "-1".to_string();
         }
     }
-    return true;
+
+    return ans.to_string();
 }
 
 #[test]
