3c6c329e552690872c2bf10a819d3827668140e5
kanno <m.kanno.0915@gmail.com>
2020-06-17 19:19:46 +0900

    AC

---
+++ examples/abc143_a.rs
@@ -0,0 +1,61 @@
+use std::io::Read;
+
+fn main() {
+    let mut buf = String::new();
+    std::io::stdin().read_to_string(&mut buf).unwrap();
+
+    let answer = solve(&buf);
+
+    println!("{}", answer);
+}
+
+fn solve(input: &str) -> String {
+    let mut iterator = input.split_whitespace();
+
+    let a: usize = iterator.next().unwrap().parse().unwrap();
+    let b: usize = iterator.next().unwrap().parse().unwrap();
+
+    return if b * 2 >= a {
+        "0".to_string()
+    } else {
+        format!("{}", a - b * 2)
+    }
+}
+
+#[test]
+fn test() {
+    let cases: Vec<(&str, &str)> = vec![
+        (
+            r"12 4",
+            "4"
+        ),
+        (
+            r"20 15",
+            "0"
+        ),
+        (
+            r"20 30",
+            "0"
+        ),
+    ];
+
+    let mut all_ok = true;
+    for (i, case) in cases.iter().enumerate() {
+        print!("case {} : ", i);
+
+        let expected = case.1;
+        let actual = solve(case.0);
+
+        if expected == actual {
+            println!("OK");
+        } else {
+            println!("NG");
+            println!("    Expected: {}", expected);
+            println!("    Actual  : {}", actual);
+
+            all_ok = false;
+        }
+    }
+
+    assert_eq!(all_ok, true);
+}
