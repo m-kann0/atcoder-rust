2739288cff5ceadc78ef2ca528bd5f67e607cb93
kanno <m.kanno.0915@gmail.com>
2020-06-20 22:18:50 +0900

    AC

---
+++ examples/abc139_a.rs
@@ -0,0 +1,65 @@
+use std::io::Read;
+
+fn main() {
+    let mut buf = String::new();
+    std::io::stdin().read_to_string(&mut buf).unwrap();
+
+    let answer = solve(&buf);
+
+    println!("{}", answer);
+}
+
+fn solve(input: &str) -> String {
+    let mut iterator = input.split_whitespace();
+
+    let s: Vec<char> = iterator.next().unwrap().chars().collect();
+    let t: Vec<char> = iterator.next().unwrap().chars().collect();
+    let mut count = 0;
+    for i in 0..3 {
+        if s[i] == t[i] {
+            count += 1;
+        }
+    }
+    return count.to_string();
+}
+
+#[test]
+fn test() {
+    let cases: Vec<(&str, &str)> = vec![
+        (
+            r"CSS
+CSR",
+            "2"
+        ),
+        (
+            r"SSR
+SSR",
+            "3"
+        ),
+        (
+            r"RRR
+SSS",
+            "0"
+        ),
+    ];
+
+    let mut all_ok = true;
+    for (i, case) in cases.iter().enumerate() {
+        print!("case {} : ", i);
+
+        let expected = case.1;
+        let actual = solve(case.0);
+
+        if expected == actual {
+            println!("OK");
+        } else {
+            println!("NG");
+            println!("    Expected: {}", expected);
+            println!("    Actual  : {}", actual);
+
+            all_ok = false;
+        }
+    }
+
+    assert_eq!(all_ok, true);
+}
