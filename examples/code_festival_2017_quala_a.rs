dbb289a6fe6297f1d179f84f470e12a664b68c6d
kanno <m.kanno.0915@gmail.com>
2020-06-26 00:30:52 +0900

    AC

---
+++ examples/code_festival_2017_quala_a.rs
@@ -0,0 +1,60 @@
+use std::io::Read;
+
+fn main() {
+    let mut buf = String::new();
+    std::io::stdin().read_to_string(&mut buf).unwrap();
+
+    let answer = solve(&buf);
+
+    println!("{}", answer);
+}
+
+fn solve(input: &str) -> String {
+    let mut iterator = input.split_whitespace();
+
+    let s = iterator.next().unwrap();
+
+    if s.starts_with("YAKI") {
+        return "Yes".to_string();
+    }
+
+    return "No".to_string();
+}
+
+#[test]
+fn test() {
+    let cases: Vec<(&str, &str)> = vec![
+        (
+            r"YAKINIKU",
+            "Yes"
+        ),
+        (
+            r"TAKOYAKI",
+            "No"
+        ),
+        (
+            r"YAK",
+            "No"
+        ),
+    ];
+
+    let mut all_ok = true;
+    for (i, case) in cases.iter().enumerate() {
+        print!("case {} : ", i);
+
+        let expected = case.1;
+        let actual = solve(case.0);
+
+        if expected == actual {
+            println!("OK");
+        } else {
+            println!("NG");
+            println!("    Expected: {}", expected);
+            println!("    Actual  : {}", actual);
+
+            all_ok = false;
+        }
+    }
+
+    assert_eq!(all_ok, true);
+}
