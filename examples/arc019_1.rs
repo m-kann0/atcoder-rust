0f6fe11714ad5815ab17d7918441587c7a0fbf3a
kanno <m.kanno.0915@gmail.com>
2020-06-27 17:23:58 +0900

    AC

---
+++ examples/arc019_1.rs
@@ -0,0 +1,70 @@
+use std::io::Read;
+
+fn main() {
+    let mut buf = String::new();
+    std::io::stdin().read_to_string(&mut buf).unwrap();
+
+    let answer = solve(&buf);
+
+    println!("{}", answer);
+}
+
+fn solve(input: &str) -> String {
+    let mut iterator = input.split_whitespace();
+
+    let s: Vec<char> = iterator.next().unwrap().chars().collect();
+
+    let mut result = String::new();
+    for i in 0..s.len() {
+        let mut c = s[i];
+        c = match c {
+            'O' => '0',
+            'D' => '0',
+            'I' => '1',
+            'Z' => '2',
+            'S' => '5',
+            'B' => '8',
+            _ => c,
+        };
+        result.push(c);
+    }
+    return result;
+}
+
+#[test]
+fn test() {
+    let cases: Vec<(&str, &str)> = vec![
+        (
+            r"1Z0",
+            "120"
+        ),
+        (
+            r"4ZD6O",
+            "42060"
+        ),
+        (
+            r"BI9Z",
+            "8192"
+        ),
+    ];
+
+    let mut all_ok = true;
+    for (i, case) in cases.iter().enumerate() {
+        print!("case {} : ", i);
+
+        let expected = case.1;
+        let actual = solve(case.0);
+
+        if expected == actual {
+            println!("OK");
+        } else {
+            println!("NG");
+            println!("    Expected: {}", expected);
+            println!("    Actual  : {}", actual);
+
+            all_ok = false;
+        }
+    }
+
+    assert_eq!(all_ok, true);
+}
