8b11d5565af51fcf57df4c787097416e5102144d
kanno <m.kanno.0915@gmail.com>
2020-06-27 18:06:13 +0900

    AC

---
+++ examples/arc052_a.rs
@@ -0,0 +1,49 @@
+use std::io::Read;
+use itertools::Itertools;
+
+fn main() {
+    let mut buf = String::new();
+    std::io::stdin().read_to_string(&mut buf).unwrap();
+
+    let answer = solve(&buf);
+
+    println!("{}", answer);
+}
+
+fn solve(input: &str) -> String {
+    let mut iterator = input.split_whitespace();
+
+    let ans = iterator.next().unwrap()
+        .chars()
+        .filter(|c| c.is_digit(10))
+        .join("");
+    return ans;
+}
+
+#[test]
+fn test() {
+    let cases: Vec<(&str, &str)> = vec![
+        (r"chokudai55", r"55"),
+        (r"cho9dai", r"9"),
+    ];
+
+    let mut all_ok = true;
+    for (i, case) in cases.iter().enumerate() {
+        print!("case {} : ", i);
+
+        let expected = case.1;
+        let actual = solve(case.0);
+
+        if expected == actual {
+            println!("OK");
+        } else {
+            println!("NG");
+            println!("    Expected: {}", expected);
+            println!("    Actual  : {}", actual);
+
+            all_ok = false;
+        }
+    }
+
+    assert_eq!(all_ok, true);
+}
