df385a2d053fa1e313d9f953ed40a01cf2b1a8b9
kanno <m.kanno.0915@gmail.com>
2020-06-28 22:55:29 +0900

    AC

---
+++ examples/mujin_pc_2016_a.rs
@@ -0,0 +1,48 @@
+use std::io::Read;
+
+fn main() {
+    let mut buf = String::new();
+    std::io::stdin().read_to_string(&mut buf).unwrap();
+
+    let answer = solve(&buf);
+
+    println!("{}", answer);
+}
+
+fn solve(input: &str) -> String {
+    let mut iterator = input.split_whitespace();
+
+    let c = iterator.next().unwrap().chars().next().unwrap();
+
+    return match c {
+        'O' | 'P' | 'K' | 'L' => "Right".to_string(),
+        _ => "Left".to_string()
+    };
+}
+
+#[test]
+fn test() {
+    let cases: Vec<(&str, &str)> = vec![
+
+    ];
+
+    let mut all_ok = true;
+    for (i, case) in cases.iter().enumerate() {
+        print!("case {} : ", i);
+
+        let expected = case.1;
+        let actual = solve(case.0);
+
+        if expected == actual {
+            println!("OK");
+        } else {
+            println!("NG");
+            println!("    Expected: {}", expected);
+            println!("    Actual  : {}", actual);
+
+            all_ok = false;
+        }
+    }
+
+    assert_eq!(all_ok, true);
+}
