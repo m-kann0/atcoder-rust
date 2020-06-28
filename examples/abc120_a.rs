b0634be7d1bd50613a8157462d9757e9f8e19002
kanno <m.kanno.0915@gmail.com>
2020-06-28 22:50:17 +0900

    AC

---
+++ examples/abc120_a.rs
@@ -0,0 +1,60 @@
+use std::io::Read;
+use std::cmp::min;
+
+fn main() {
+    let mut buf = String::new();
+    std::io::stdin().read_to_string(&mut buf).unwrap();
+
+    let answer = solve(&buf);
+
+    println!("{}", answer);
+}
+
+fn solve(input: &str) -> String {
+    let mut iterator = input.split_whitespace();
+
+    let a: usize = iterator.next().unwrap().parse().unwrap();
+    let b: usize = iterator.next().unwrap().parse().unwrap();
+    let c: usize = iterator.next().unwrap().parse().unwrap();
+
+    let ans = min(b / a, c);
+    return ans.to_string();
+}
+
+#[test]
+fn test() {
+    let cases: Vec<(&str, &str)> = vec![
+        (
+            r"2 11 4",
+            "4"
+        ),
+        (
+            r"3 9 5",
+            "3"
+        ),
+        (
+            r"100 1 10",
+            "0"
+        ),
+    ];
+
+    let mut all_ok = true;
+    for (i, case) in cases.iter().enumerate() {
+        print!("case {} : ", i);
+
+        let expected = case.1;
+        let actual = solve(case.0);
+
+        if expected == actual {
+            println!("OK");
+        } else {
+            println!("NG");
+            println!("    Expected: {}", expected);
+            println!("    Actual  : {}", actual);
+
+            all_ok = false;
+        }
+    }
+
+    assert_eq!(all_ok, true);
+}
