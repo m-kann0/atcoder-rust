d515cc8b7af0d953fa2d48f0c4579c44c608e7cb
kanno <m.kanno.0915@gmail.com>
2020-06-29 00:24:53 +0900

    AC

---
+++ examples/abc086_a.rs
@@ -0,0 +1,57 @@
+use std::io::Read;
+
+fn main() {
+    let mut buf = String::new();
+    std::io::stdin().read_to_string(&mut buf).unwrap();
+
+    let answer = solve(&buf);
+
+    println!("{}", answer);
+}
+
+fn solve(input: &str) -> String {
+    let mut iterator = input.split_whitespace();
+
+    let a: usize = iterator.next().unwrap().parse().unwrap();
+    let b: usize = iterator.next().unwrap().parse().unwrap();
+
+    return if (a * b) % 2 == 0 {
+        "Even".to_string()
+    } else {
+        "Odd".to_string()
+    };
+}
+
+#[test]
+fn test() {
+    let cases: Vec<(&str, &str)> = vec![
+        (
+            r"3 4",
+            "Even"
+        ),
+        (
+            r"1 21",
+            "Odd"
+        ),
+    ];
+
+    let mut all_ok = true;
+    for (i, case) in cases.iter().enumerate() {
+        print!("case {} : ", i);
+
+        let expected = case.1;
+        let actual = solve(case.0);
+
+        if expected == actual {
+            println!("OK");
+        } else {
+            println!("NG");
+            println!("    Expected: {}", expected);
+            println!("    Actual  : {}", actual);
+
+            all_ok = false;
+        }
+    }
+
+    assert_eq!(all_ok, true);
+}
