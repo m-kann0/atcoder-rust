0540993a1ac8d5e17f1d7e7cd9c0fad00211a4c9
kanno <m.kanno.0915@gmail.com>
2020-06-30 00:34:26 +0900

    AC

---
+++ examples/abc098_a.rs
@@ -0,0 +1,58 @@
+use std::io::Read;
+use std::cmp::max;
+
+fn main() {
+    let mut buf = String::new();
+    std::io::stdin().read_to_string(&mut buf).unwrap();
+
+    let answer = solve(&buf);
+
+    println!("{}", answer);
+}
+
+fn solve(input: &str) -> String {
+    let mut iterator = input.split_whitespace();
+
+    let a: isize = iterator.next().unwrap().parse().unwrap();
+    let b: isize = iterator.next().unwrap().parse().unwrap();
+
+    return max(a + b, max(a - b, a * b)).to_string();
+}
+
+#[test]
+fn test() {
+    let cases: Vec<(&str, &str)> = vec![
+        (
+            r"3 1",
+            "4"
+        ),
+        (
+            r"4 -2",
+            "6"
+        ),
+        (
+            r"0 0",
+            "0"
+        ),
+    ];
+
+    let mut all_ok = true;
+    for (i, case) in cases.iter().enumerate() {
+        print!("case {} : ", i);
+
+        let expected = case.1;
+        let actual = solve(case.0);
+
+        if expected == actual {
+            println!("OK");
+        } else {
+            println!("NG");
+            println!("    Expected: {}", expected);
+            println!("    Actual  : {}", actual);
+
+            all_ok = false;
+        }
+    }
+
+    assert_eq!(all_ok, true);
+}
