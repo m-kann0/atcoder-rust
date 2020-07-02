afd3eb17805a1a6ecd55d8d2b305efc7a1994726
kanno <m.kanno.0915@gmail.com>
2020-07-03 00:58:30 +0900

    AC

---
+++ examples/arc020_1.rs
@@ -0,0 +1,65 @@
+use std::io::Read;
+
+fn main() {
+    let mut buf = String::new();
+    std::io::stdin().read_to_string(&mut buf).unwrap();
+
+    let answer = solve(&buf);
+
+    println!("{}", answer);
+}
+
+fn solve(input: &str) -> String {
+    let mut iterator = input.split_whitespace();
+
+    let a: isize = iterator.next().unwrap().parse().unwrap();
+    let b: isize = iterator.next().unwrap().parse().unwrap();
+    let a = a.abs();
+    let b = b.abs();
+
+    if a < b {
+        "Ant".to_string()
+    } else if a > b {
+        "Bug".to_string()
+    } else {
+        "Draw".to_string()
+    }
+}
+
+#[test]
+fn test() {
+    let cases: Vec<(&str, &str)> = vec![
+        (
+            r"2 3",
+            "Ant"
+        ),
+        (
+            r"1 0",
+            "Bug"
+        ),
+        (
+            r"-100 100",
+            "Draw"
+        ),
+    ];
+
+    let mut all_ok = true;
+    for (i, case) in cases.iter().enumerate() {
+        print!("case {} : ", i);
+
+        let expected = case.1;
+        let actual = solve(case.0);
+
+        if expected == actual {
+            println!("OK");
+        } else {
+            println!("NG");
+            println!("    Expected: {}", expected);
+            println!("    Actual  : {}", actual);
+
+            all_ok = false;
+        }
+    }
+
+    assert_eq!(all_ok, true);
+}
