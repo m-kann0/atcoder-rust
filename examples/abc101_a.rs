6fb9c52317d18169e335f36672ce933c3c5f787d
kanno <m.kanno.0915@gmail.com>
2020-07-05 20:37:13 +0900

    AC

---
+++ examples/abc101_a.rs
@@ -0,0 +1,64 @@
+use std::io::Read;
+
+fn main() {
+    let mut buf = String::new();
+    std::io::stdin().read_to_string(&mut buf).unwrap();
+
+    let answer = solve(&buf);
+
+    println!("{}", answer);
+}
+
+fn solve(input: &str) -> String {
+    let mut iterator = input.split_whitespace();
+
+    let s: Vec<char> = iterator.next().unwrap().chars().collect();
+
+    let mut ans: isize = 0;
+    for c in s {
+        if c == '+' {
+            ans += 1;
+        } else {
+            ans -= 1;
+        }
+    }
+    ans.to_string()
+}
+
+#[test]
+fn test() {
+    let cases: Vec<(&str, &str)> = vec![
+        (
+            r"+-++",
+            "2"
+        ),
+        (
+            r"-+--",
+            "-2"
+        ),
+        (
+            r"----",
+            "-4"
+        ),
+    ];
+
+    let mut all_ok = true;
+    for (i, case) in cases.iter().enumerate() {
+        print!("case {} : ", i);
+
+        let expected = case.1;
+        let actual = solve(case.0);
+
+        if expected == actual {
+            println!("OK");
+        } else {
+            println!("NG");
+            println!("    Expected: {}", expected);
+            println!("    Actual  : {}", actual);
+
+            all_ok = false;
+        }
+    }
+
+    assert_eq!(all_ok, true);
+}
