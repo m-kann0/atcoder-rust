7c31cf890912f754ad029e13871de696edaeda29
kanno <m.kanno.0915@gmail.com>
2020-07-30 01:27:00 +0900

    AC

---
+++ examples/caddi2018_b.rs
@@ -0,0 +1,62 @@
+use std::io::Read;
+
+fn main() {
+    let mut buf = String::new();
+    std::io::stdin().read_to_string(&mut buf).unwrap();
+
+    let answer = solve(&buf);
+
+    println!("{}", answer);
+}
+
+fn solve(input: &str) -> String {
+    let mut iterator = input.split_whitespace();
+
+    let n: usize = iterator.next().unwrap().parse().unwrap();
+    let a: Vec<usize> = (0..n).map(|_| iterator.next().unwrap().parse().unwrap()).collect();
+
+    if a.iter().all(|it| *it % 2 == 0) {
+        "second".to_string()
+    } else {
+        "first".to_string()
+    }
+}
+
+#[test]
+fn test() {
+    let cases: Vec<(&str, &str)> = vec![
+        (
+            r"2
+1
+2",
+            "first"
+        ),
+        (
+            r"3
+100000
+30000
+20000",
+            "second"
+        ),
+    ];
+
+    let mut all_ok = true;
+    for (i, case) in cases.iter().enumerate() {
+        print!("case {} : ", i);
+
+        let expected = case.1;
+        let actual = solve(case.0);
+
+        if expected == actual {
+            println!("OK");
+        } else {
+            println!("NG");
+            println!("    Expected: {}", expected);
+            println!("    Actual  : {}", actual);
+
+            all_ok = false;
+        }
+    }
+
+    assert_eq!(all_ok, true);
+}
