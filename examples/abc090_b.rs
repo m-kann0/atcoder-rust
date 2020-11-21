6d565a56d379bdc2ebdb689911b12dbb6a0cce27
kanno <m.kanno.0915@gmail.com>
2020-11-22 08:22:21 +0900

    AC

---
+++ examples/abc090_b.rs
@@ -0,0 +1,62 @@
+#![allow(non_snake_case)]
+
+use std::io::Read;
+
+fn main() {
+    let mut buf = String::new();
+    std::io::stdin().read_to_string(&mut buf).unwrap();
+
+    let answer = solve(&buf);
+
+    println!("{}", answer);
+}
+
+fn solve(input: &str) -> String {
+    let mut iterator = input.split_whitespace();
+
+    let a: usize = iterator.next().unwrap().parse().unwrap();
+    let b: usize = iterator.next().unwrap().parse().unwrap();
+
+    let mut ans: usize = 0;
+    for n in a..=b {
+        let s: Vec<char> = n.to_string().chars().collect();
+        if s[0] == s[4] && s[1] == s[3] {
+            ans += 1;
+        }
+    }
+    ans.to_string()
+}
+
+#[test]
+fn test() {
+    let cases: Vec<(&str, &str)> = vec![
+        (
+            r"11009 11332",
+            "4"
+        ),
+        (
+            r"31415 92653",
+            "612"
+        ),
+    ];
+
+    let mut all_ok = true;
+    for (i, case) in cases.iter().enumerate() {
+        print!("case {} : ", i);
+
+        let expected = case.1;
+        let actual = solve(case.0);
+
+        if expected == actual {
+            println!("OK");
+        } else {
+            println!("NG");
+            println!("    Expected: {}", expected);
+            println!("    Actual  : {}", actual);
+
+            all_ok = false;
+        }
+    }
+
+    assert_eq!(all_ok, true);
+}
