a3b88d2d6b4e5a63239b3c6763ae9b3731cd584a
kanno <m.kanno.0915@gmail.com>
2020-12-16 07:26:58 +0900

    AC

---
+++ examples/abc100_c.rs
@@ -0,0 +1,70 @@
+#![allow(non_snake_case)]
+
+use std::io::Read;
+
+fn main() {
+    let mut buf = String::new();
+    std::io::stdin().read_to_string(&mut buf).unwrap();
+
+    let answer = solve(&buf);
+
+    println!("{}", answer);
+}
+
+fn solve(input: &str) -> String {
+    let mut iterator = input.split_whitespace();
+
+    let n: usize = iterator.next().unwrap().parse().unwrap();
+    let a: Vec<usize> = (0..n).map(|_| iterator.next().unwrap().parse().unwrap()).collect();
+
+    let mut ans = 0_usize;
+    for i in 0..n {
+        let mut x = a[i];
+        while x > 0 && x % 2 == 0 {
+            ans += 1;
+            x /= 2;
+        }
+    }
+    ans.to_string()
+}
+
+#[test]
+fn test() {
+    let cases: Vec<(&str, &str)> = vec![
+        (
+            r"3
+5 2 4",
+            "3"
+        ),
+        (
+            r"4
+631 577 243 199",
+            "0"
+        ),
+        (
+            r"10
+2184 2126 1721 1800 1024 2528 3360 1945 1280 1776",
+            "39"
+        ),
+    ];
+
+    let mut all_ok = true;
+    for (i, case) in cases.iter().enumerate() {
+        print!("case {} : ", i);
+
+        let expected = case.1;
+        let actual = solve(case.0);
+
+        if expected.trim() == actual.trim() {
+            println!("OK");
+        } else {
+            println!("NG");
+            println!("    Expected: {}", expected);
+            println!("    Actual  : {}", actual);
+
+            all_ok = false;
+        }
+    }
+
+    assert_eq!(all_ok, true);
+}
